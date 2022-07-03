2c8063171d9f13f37b869f15c78e7b501f157aa9
Fuyh <fuyh_20@163.com>
2022-07-03 13:38:53 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,5 @@
+## 暑培第二讲作业
+
+*   fork作业仓库https://github.com/fuyh20/Git-Homework到自己的仓库
+*   在dev分支上按提示更改hw.txt，然后将更改后的文件push到自己仓库的dev分支
+*   尝试提pr将自己仓库dev分支合并到作业仓库的main分支（注：一般不会这么做，但是为了作业www）
---
+++ check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+content=$(cat ./hw.txt)
+lines=(0 0 0 0)
+i=0
+
+for line in $content
+do
+    lines[$i]=$line
+    i=$((i+1))
+done
+
+ans=$((${lines[1]: -1} * ${lines[2]: -1}))
+res=$(echo ${lines[3]} | tr -cd "[0-9]")
+# echo $ans
+# echo $res
+
+if [ $ans != $res ]
+then
+    exit 1
+else
+    echo "correct"
+fi
